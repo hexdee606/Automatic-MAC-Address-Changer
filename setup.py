5183971f5ba5bf85a6d2c4c24e28dd332a00140e
Automatic-MAC-Address-Changer <dipenc245@gmail.com>
2021-07-17 22:54:19 +0530

    New Commit 10:54

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='amc-v1',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/hexdee606/auto_mac_change',
+    license='Apache License, Version 2.0',
+    author='hexdee606',
+    author_email='hexdee606@gmail.com',
+    description='Automatic Media Access Control [MAC] Address Changing Script.'
+)
